b56555ab70e4b7bcfd5a91dcf816e85741aba303
aliseyedi <solafiali657@gmail.com>
2023-03-12 22:28:59 +0330

    add FontAwesome Icon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import "./App.css";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck, faPen, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return <div></div>;
