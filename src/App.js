e01b8a77e6942f482cd6273e8fa1397a54f386aa
aliseyedi <solafiali657@gmail.com>
2023-03-13 16:21:09 +0330

    add new task component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import ToDoTask from "./components/ToDoTask";
+import AddTask from "./components/AddTask";
 
 function App() {
+  // State
   const [toDo, setToDo] = useState([
     { id: 1, title: "book1", status: false },
     { id: 2, title: "book2", status: true },
   ]);
+
+  const [newTask, setNewTask] = useState("");
 
   // check Done Task
   const CheckTask = (id) => {
@@ -26,11 +30,25 @@ function App() {
     setToDo(newTask);
   };
 
+  // Add Task
+  const addTask = () => {
+    if (newTask) {
+      let num = toDo.length + 1;
+      let newEntry = { id: num, title: newTask, status: false };
+      setToDo([...toDo, newEntry]);
+      setNewTask("");
+    }
+  };
+
   return (
-    <div className="container App  d-flex justify-content-center align-items-center   ">
-      <div className=" d-flex flex-column justify-content-center align-items-center w-50 ">
+    <div className=" vh-100 vw-100  d-flex justify-content-center align-items-center bg-success  ">
+      <div className=" w-50 d-flex flex-column justify-content-center align-items-center  ">
+        {/* Add Task */}
+        <AddTask newTask={newTask} setNewTask={setNewTask} addTask={addTask} />
+
         {/* No Task Warning */}
         {toDo && toDo.length ? "" : "No Task ..."}
+
         {/* ToDo Task */}
         <ToDoTask toDo={toDo} checkTask={CheckTask} delTask={DeleteTask} />
       </div>
