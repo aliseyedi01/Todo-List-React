ca041d6c392d0f16fe9594fc9b15c8f2b0ae89f5
aliseyedi <solafiali657@gmail.com>
2023-03-13 13:42:26 +0330

    add done task

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleCheck, faPen, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
 import ToDoTask from "./components/ToDoTask";
 
 function App() {
   const [toDo, setToDo] = useState([
-    { id: 1, title: "book1", status: true },
+    { id: 1, title: "book1", status: false },
     { id: 2, title: "book2", status: true },
   ]);
+
+  // check Done Task
+  const CheckTask = (id) => {
+    let newTask = toDo.map((task) => {
+      if (task.id === id) {
+        return { ...task, status: !task.status };
+      }
+      return task;
+    });
+    setToDo(newTask);
+  };
 
   return (
     <div className="container App  d-flex justify-content-center align-items-center   ">
       <div className=" d-flex flex-column justify-content-center align-items-center w-50 ">
-        <ToDoTask toDo={toDo} />
+        <ToDoTask toDo={toDo} checkTask={CheckTask} />
       </div>
     </div>
   );
--- src/components/ToDoTask.jsx
+++ src/components/ToDoTask.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleCheck, faPen, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
-const ToDoTask = ({ toDo }) => {
+const ToDoTask = ({ toDo, checkTask }) => {
   return (
     <>
       {toDo &&
@@ -15,21 +15,29 @@ const ToDoTask = ({ toDo }) => {
               <React.Fragment key={task.id}>
                 <div className="d-flex w-50 flex-row justify-content-between align-content-center p-2 text-bg-primary m-1 rounded">
                   {/* Left */}
-                  <div className="d-flex flex-row align-items-center gap-1">
-                    <span className="rounded text-bg-light px-1 py-0">{index + 1}</span>
-                    <span className="text-capitalize">{task.title}</span>
+                  <div className="d-flex flex-row align-items-center ">
+                    <div className={task.status ? "text-decoration-line-through , opacity-50" : ""}>
+                      <span className="rounded text-bg-light px-1 py-0 ">{index + 1}</span>
+                      <span className="px-1 text-capitalize">{task.title}</span>
+                    </div>
                   </div>
                   {/* right */}
-                  <div className="d-flex flex-row gap-1">
-                    <span title="Done">
+                  <div className="d-flex flex-row gap-2 ">
+                    <span
+                      title="Done"
+                      role="button"
+                      onClick={() => {
+                        checkTask(task.id);
+                      }}
+                    >
                       <FontAwesomeIcon icon={faCircleCheck} />
                     </span>
 
-                    <span title="Edit">
+                    <span title="Edit" role="button">
                       <FontAwesomeIcon icon={faPen} />
                     </span>
 
-                    <span title="Remove">
+                    <span title="Remove" role="button">
                       <FontAwesomeIcon icon={faTrashCan} />
                     </span>
                   </div>
